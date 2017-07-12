abdf0bfc8e385f3ce77f7ae0af734ae59426de79
Dhruv <dg28gadhavi@gmail.com>
2017-07-13 05:18:30 +0530

    Updated Battery Capacity To 3300mAh

--- drivers/power/mediatek/battery_common_fg_20.c
+++ drivers/power/mediatek/battery_common_fg_20.c
@@ -268,6 +268,8 @@ struct battery_data {
 	int capacity_smb;
 	int present_smb;
 	int adjust_power;
+    int charge_full_design;
+    int charge_full;
 };
 
 static enum power_supply_property wireless_props[] = {
@@ -305,6 +307,8 @@ static enum power_supply_property battery_props[] = {
 	POWER_SUPPLY_PROP_present_smb,
 	/* ADB CMD Discharging */
 	POWER_SUPPLY_PROP_adjust_power,
+	POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN,
+    POWER_SUPPLY_PROP_CHARGE_FULL,
 };
 
 struct timespec batteryThreadRunTime;
@@ -625,6 +629,13 @@ static int battery_get_property(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_adjust_power:
 		val->intval = data->adjust_power;
 		break;
+
+	case POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN:
+ 		val->intval = data->charge_full_design;
+ 		break;
+ 	case POWER_SUPPLY_PROP_CHARGE_FULL:
+ 		val->intval = data->charge_full;
+ 		break;
 
 	default:
 		ret = -EINVAL;
@@ -1648,7 +1659,8 @@ static void battery_update(struct battery_data *bat_data)
 	bat_data->BAT_batt_vol = BMT_status.bat_vol;
 	bat_data->BAT_batt_temp = BMT_status.temperature * 10;
 	bat_data->BAT_PRESENT = BMT_status.bat_exist;
-
+	bat_data->charge_full_design = 3300 * 1000;
+	bat_data->charge_full = 3300 * 1000;
 
 	if ((BMT_status.charger_exist == KAL_TRUE) && (BMT_status.bat_charging_state != CHR_ERROR)) {
 		if (BMT_status.bat_exist) {
