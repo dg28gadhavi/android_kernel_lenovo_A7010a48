36d3426d305cc7850c3e7bd8bcab83fd2013fba4
dg28gadhavi <dg28gadhavi@gmail.com>
2017-07-17 17:04:47 +0000

    Added build script

--- Negative.sh
+++ Negative.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo " Negative kernel by DG "
-export KBUILD_BUILD_USER="DG"
+export KBUILD_BUILD_USER="Dhruv"
 export KBUILD_BUILD_HOST="Negative"
-export CROSS_COMPILE="/home/negative/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+export CROSS_COMPILE="/home/darkness/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
 
 export ARCH=arm64
 
