ae56ec94a014e2a78212d19e72124385d13e2a29
DHRUV GADHAVI <dg28gadhavi@gmail.com>
2017-07-23 00:45:03 +0530

    Updated build script for standalone compilation

--- Negative.sh
+++ Negative.sh
@@ -6,8 +6,9 @@ export CROSS_COMPILE="/home/darkness/aarch64-linux-android-4.9/bin/aarch64-linux
 
 export ARCH=arm64
 
-make clean && make mrproper
+make clean O=out/
+make mrproper O=out/
 
-make k5fpr_defconfig
+make k5fpr_defconfig O=out/
 
-make -j8
+make -j8 O=out/
